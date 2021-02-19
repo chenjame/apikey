ce83be174c8ac5206f046f1ab6db7218a1d625a8
Kent Hsieh <hsiehk98@gmail.com>
2021-02-19 01:11:25 -0500

    GUI_test to GUI_execute update gui with score

--- python/GUI_execute.py
+++ python/GUI_execute.py
@@ -4,19 +4,22 @@ import createDataSet as d
 import assignScore as a
 
 def main_function():
+    global userScoreText
     url = str(E1.get())
     did = d.did_from_url(url)
     try: 
         idList = d.userWIDEID(did)
     except AttributeError: 
-        # please enter a correct url
+        userScoreText = "Uh Oh, Please Input Correct URL"
+        userScoreLabel.config(text = userScoreText)
     userData = d.createTestSet(idList)
     userScore = a.assignScore("SampleDataset.csv", userData)
-
+    userScoreText = userScore
+    userScoreLabel.config(text = userScore)
     return
 
 gui = tk.Tk()
-gui.geometry('300x100')
+gui.geometry('300x110')
 L1 = tk.Label(gui, text="Insert Part URL from Onshape: ")
 L1.pack(side = "top")
 E1 = tk.Entry(gui, bd =5, width = 35)
@@ -24,9 +27,10 @@ E1.pack(side = "top")
 B1 = tk.Button(gui, text =" Enter ", command = main_function)
 B1.pack(side = "top")
 scoreLabel = tk.Label(gui, text ="SCORE: ")
-scoreLabel.pack(side = "bottom")
-userScoreLabel = tk.Label(gui, text="---------")
-userScoreLabel.pack(side="bottom")
+scoreLabel.pack(fill = "x")
+userScoreText = " -------------- "
+userScoreLabel = tk.Label(gui,text = userScoreText)
+userScoreLabel.pack(side = "bottom")
 
 
 gui.mainloop()
